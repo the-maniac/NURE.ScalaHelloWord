e3ce24b0318d8702381b06e3257a8a6b1085a286
the-maniac <yura.pilguk@gmail.com>
2019-11-28 22:55:29 +0200

    Added timer

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -2,19 +2,26 @@ import akka.actor.Actor
 import akka.actor.ActorSystem
 import akka.actor.Props
 import akka.actor.ActorRef
+import akka.actor.Timers
+import scala.concurrent.duration._
 
 case object PingMessage
 case object PongMessage
+case object MyTickKey
+case object MyTickPong
 case object StartMessage
 case object StopMessage
 
-class Ping(pong: ActorRef) extends Actor {
+class Ping(pong: ActorRef) extends Actor with Timers {
     var count = 0
+    var a : ActorRef = null
     def incrementAndPrint { count += 1; println("ping"); println("Message Number: " + count.toString); }
     def receive = {
         case StartMessage =>
             incrementAndPrint
             pong ! PingMessage
+        case MyTickPong =>
+            a ! PingMessage
         case PongMessage =>
             incrementAndPrint
             if (count > 99) {
@@ -22,7 +29,9 @@ class Ping(pong: ActorRef) extends Actor {
                 println("ping stopped")
                 context.stop(self)
             } else {
-                sender ! PingMessage
+                a = sender()
+                timers.startTimerWithFixedDelay(MyTickKey, MyTickPong, 500.milliseconds)
+                println("waiting before sending ping message")
             }
         case _ => println("Ping got something unexpected.")
     }
